b5942ee72b43efb55d97492b0ffa65b3460b5d0f
Sadat Hussain <sadat.h@aiolos.cloud>
2022-07-27 19:30:17 +0530

    Part 2: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -64,4 +64,6 @@ public class Restaurant {
         return name;
     }
 
+    
+
 }
--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -5,11 +5,19 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException{
+        Restaurant restaurantMatched=null;
+        
         for(Restaurant restaurant:restaurants){
-            if(restaurant.getName().toLowerCase().equals(restaurantName.toLowerCase()));
+            if(restaurant.getName().toLowerCase().equals(restaurantName.toLowerCase())){
+                restaurantMatched= restaurant;
+            };
+               
         }
-        return null;
+        if(restaurantMatched==null){
+            throw new restaurantNotFoundException(restaurantName);
+        }
+        return restaurantMatched;
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
 
